5230631996e2ffc0a48666240dbf1a8ea4b7c1b4
MeliRobu <melisofirb@gmail.com>
2026-03-16 20:46:17 -0500

    Add files via upload

---
+++ Inventory.py
@@ -0,0 +1,89 @@
+import os
+os.system("clear")
+# INVENTORY
+# This script is a welcome message that initialize the whole program
+print("\t***HI! Here you can ingress any product and update your inventory***")
+
+# In line 7, is defined a function named "product()", is responsible of request to the user the product name
+# It contains conditions to handle typing errors 
+def product():
+    product_name=(input("\no) Type the product name: "))
+    if product_name.isdigit() or product_name =="" or product_name ==" ":
+        print("\nYou have to insert a product name, please try again.")
+        return product()
+    else:
+        return product_name
+    
+#Here we invoke the function "product()"
+name_product=product()
+
+# In line 19, is defined a function named "price()", is responsible of request to the user the product price
+# It contains a "Try - expect" function to handle data type errors. 
+def price ():
+    try:
+        product_price= float(input("o) Type the product price: "))
+        if product_price <=0:
+            print("The price cannot be negative")
+            return price()
+        else:
+            return product_price
+    except ValueError:
+        print("Please type a number")
+        return price()
+    
+# In line 33, is defined a function named "amount()", is responsible of request to the user the product amount
+# It contains a "Try- expect" function in order to to handle data type errors. 
+def amount():
+    try:
+        product_amount= float(input("o) Type the product amount: "))
+        if product_amount <0:
+            print("The price cannot be negative")
+            return amount()
+        return product_amount
+    except ValueError:
+        print("Please type a number")
+        return amount()
+
+#In line 45 and 46 this 2 new variables store function results 
+amount_variable=amount()
+price_variable=price()
+#This script calculates total amount 
+total_amount= amount_variable*price_variable
+
+#In line 52 is defined a function named dictionary() that returns the keys and the values of each asked variable above.
+#This creates an organized data storage.
+dictionary= {
+        "product_name":name_product,
+        "product_price":price_variable,
+        "producto_amount":amount_variable,
+        "total": total_amount
+    }
+
+#This script contains a empthy list, in order to full it according to the given information.
+#In line 67, the dicionary is added to the list, using ".append()" function.
+product_list = []
+product_list.append(dictionary())
+
+#This script displays the invoice 
+print(f"\n\t***INVOICE*** \nThe product name is: {name_product} \nThe product amount is: {amount_variable} \nTge product price is: {price_variable} \nThe total amount is: {total_amount} ")
+
+#This script ask if the user what to continue ingressing products
+another_product= input("Do you want to ingress another product? , type yes or no: ")
+
+#This script parses the user answer (yes or no), through a while, so, the user can ingress whenever product while aswering yes
+while another_product== "yes":
+    name_product=product()
+    amount_variable=amount()
+    price_variable=price()
+    total_amount= amount_variable*price_variable
+    product_list.append(dictionary())
+    print(f"\n\t***INVOICE*** \nThe product name is: {name_product} \nThe product amount is: {amount_variable} \nTge product price is: {price_variable} \nThe total amount is: {total_amount} ")
+    another_product= input("Do you want to ingress another product? , type yes or no: ")
+    
+#This script displays a finished message, if the while loop is break
+print("\n\t***Process finished, have a nice day***")
+        
+#This script displays the product list, with all the entered inventory items.
+print(f"\n\***INVENTORY*** \n{product_list}")
+
+#con un for recorro la lista y la puedo enumerar
